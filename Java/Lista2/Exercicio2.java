51fe8624c4c1da63177dec361d1d46e8b48fb6a9
jesilSilva <38405767+jesilSilva@users.noreply.github.com>
2018-05-25 22:02:40 -0300

    Update Exercicio2.java

--- Java/Lista2/Exercicio2.java
+++ Java/Lista2/Exercicio2.java
@@ -1,49 +1,46 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Lista2;
 
 import java.util.Scanner;
 
 /**
  *
- * @author Carlos Antonio
+ * @author Jesiel Silva
  */
 public class Exercicio2 {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        Scanner leitura = new Scanner(System.in);
-        float l1, l2, l3;
-        System.out.println("Informe Lado 1");
-        l1 = leitura.nextFloat();
-        System.out.println("Informe Lado ");
-        l2 = leitura.nextFloat();
-        System.out.println("Informe Lado 3");
-        l3 = leitura.nextFloat();
-        if ((l1 + l2 > l3) && (l2 + l3 > l1) && (l3 + l1 > l2)) {
-            //( L1+L2 > L3; L2+L3 >L1 e L3+L1 > L2)  
-            System.out.println("Os Lados Informado formam um Triangulo");
+        
+        Scanner guardar = new Scanner(System.in);
+        
+        float lado1, lado2, lado3;
+        
+        System.out.println("Informe 1° lado:");
+        lado1 = guardar.nextFloat();
+        System.out.println("Informe 2° lado: ");
+        lado2 = guardar.nextFloat();
+        System.out.println("Informe 3° lado: ");
+        lado3 = guardar.nextFloat();
+        
+        if ((lado1 + lado2 > lado3) && (lado2 + lado3 > lado1) && (lado3 + lado1 > lado2)) {
+            
+            System.out.println("Os Lados Informados formam um Triângulo!");
         } else {
-            System.out.println("Os Lados informado não formam um Trianguro");
+            System.out.println("Os Lados informados não formam um Triângulo!");
         }
 
-        if ((l1 == l2 && l1 != l3) || (l1 == l3 && l1 != l2) || (l3 == l2 && l3 != l1)) {
-            System.out.println("O Triangulo é ISOCELES");
+        if ((lado1 == lado2 && lado1 != lado3) || (lado1 == lado3 && lado1 != lado2) || (lado3 == lado2 && lado3 != lado1)) {
+            System.out.println("O triângulo é (ISOCELES)");
 
-        } else if ((l1 != l3) && (l1 != l2) && (l3 != l1)) {
-            System.out.println("O Triangulo é ESCALENO");
+        } else if ((lado1 != lado3) && (lado1 != lado2) && (lado3 != lado1)) {
+            System.out.println("O triângulo é (ESCALENO)");
 
-        } else if ((l1 == l2 && l1 == l3)) {
-            System.out.println("O Triangulo é IQUILATERO");
+        } else if ((lado1 == lado2 && lado1 == lado3)) {
+            System.out.println("O triângulo é (IQUILATERO)");
 
         }
 
     }
 
-    // TODO code application logic here
+    
 }
