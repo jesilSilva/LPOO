16c1c08c503bb9b041165cb81462b7763c6f2ddc
jesilSilva <38405767+jesilSilva@users.noreply.github.com>
2018-06-02 16:00:45 -0300

    Add files via upload

---
+++ Java/ListaExercicioCap3/Exercicio4/Empregado.java
@@ -0,0 +1,60 @@
+package Exercicio4;
+
+public class Empregado {
+
+    private String primeiroNome;
+    private String sobrenome;
+    private Double salarioMensal;
+
+    public String getPrimeiroNome() {
+        return primeiroNome;
+    }
+
+    public void setPrimeiroNome(String primeiroNome) {
+        this.primeiroNome = primeiroNome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public Double getSalarioMensal() {
+        return salarioMensal;
+    }
+
+    public void setSalarioMensal(Double salarioMensal) {
+        this.salarioMensal = salarioMensal;
+    }
+
+    public Double salarioAnual(Double salarioMensal) {
+        if (salarioMensal < 0) {
+            salarioMensal = 0.0;
+        }
+        Double salarioAnual = salarioMensal * 12;
+        return salarioAnual;
+    }
+
+    public Double aumento(Double salarioMensal) {
+        if (salarioMensal < 0) {
+            salarioMensal = 0.0;
+        }
+       
+        Double aumento=0.10;
+        Double salarioAumentado=(salarioMensal*aumento)+salarioMensal;
+       
+        return salarioAumentado*12;
+    }
+
+    public void dadosEmpregados(){
+        System.out.println("Dados do empregado:");
+        System.out.println("Nome: " + getPrimeiroNome());
+        System.out.println("Sobrenome: " +getSobrenome());
+        System.out.println("Salário anual sem aumento: R$ " +salarioAnual(salarioMensal));
+        System.out.println("Salário anual com aumento de 10%: R$ " +aumento(salarioMensal));
+
+    }
+}
---
+++ Java/ListaExercicioCap3/Exercicio4/EmpregadoTeste.java
@@ -0,0 +1,51 @@
+package Exercicio4;
+
+import java.util.Scanner;
+
+public class EmpregadoTeste {
+
+    public static void main(String[] args) {
+
+        Empregado empregado1 = new Empregado();
+        Scanner guardar = new Scanner(System.in);
+
+        System.out.println("Informe os dados do empregado 1");
+        System.out.println("Informe o primeiro nome:");
+        String nome = guardar.next();
+        empregado1.setPrimeiroNome(nome);
+
+        System.out.println("Informe o sobrenome:");
+        String sobrenome = guardar.next();
+        empregado1.setSobrenome(sobrenome);
+
+        System.out.println("Informe o salário:");
+        Double salarioMensal = guardar.nextDouble();
+        empregado1.setSalarioMensal(salarioMensal);
+
+        System.out.println("Salário anual sem aumento do empregado 1: R$ " + empregado1.salarioAnual(salarioMensal)+"\n");
+
+        Empregado empregado2 = new Empregado();
+
+        System.out.println("Informe os dados do empregado 2");
+        System.out.println("Informe o primeiro nome:");
+        nome = guardar.next();
+        empregado2.setPrimeiroNome(nome);
+
+        System.out.println("Informe o sobrenome:");
+        sobrenome = guardar.next();
+        empregado2.setSobrenome(sobrenome);
+
+        System.out.println("Informe o salário:");
+        salarioMensal = guardar.nextDouble();
+        empregado2.setSalarioMensal(salarioMensal);
+
+        System.out.println("Salário anual sem aumento do empregado 2: R$ " + empregado2.salarioAnual(salarioMensal)+"\n");
+
+        System.out.println("Os dados do empregado 1 com aumento de 10%:");
+        empregado1.dadosEmpregados();
+        System.out.println("");
+        System.out.println("Os dados do empregado 2 com aumento de 10%:");
+        empregado2.dadosEmpregados();
+    }
+
+}
