d3d6e3e32d86ac50d402d831a691e2874759a1a7
jesilSilva <38405767+jesilSilva@users.noreply.github.com>
2018-06-19 16:27:57 -0300

    Add files via upload

---
+++ Java/Lista 4 - Vetores e Matrizes/Funcionario.java
@@ -0,0 +1,25 @@
+package vetoresmatrizes;
+
+/**
+ *
+ * @author Jesiel Carlos
+ */
+public class Funcionario {
+    
+    int matricula;
+    String nome;
+    int departamento;
+    float salario;
+    String funcao;
+
+    public Funcionario(int matricula, String nome, int departamento, float salario, String funcao) {
+        this.matricula = matricula;
+        this.nome = nome;
+        this.departamento = departamento;
+        this.salario = salario;
+        this.funcao = funcao;
+    }
+    
+    
+    
+}
